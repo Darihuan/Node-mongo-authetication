c89ab9faac0928ebfd8a25ff982e1eb3fe90d423
Daniel Mesa <damesamarin@gmail.com>
2021-08-25 16:38:45 +0200

    puerto cambiado

--- src/index.js
+++ src/index.js
@@ -5,13 +5,13 @@ const express= require('express');
 
 /*inicializaciones*/
 const app=express();
-const _PORT=1203;
+const $PORT=443;
 const mongo=require('./database');
 app.use(express.json());
 app.use(require('./endpoints/index'));
 /*servidor*/
 
-app.listen(_PORT,()=>{
+app.listen($PORT,()=>{
     console.log("=====================")
     console.log("SERVER ON PORT:",_PORT)
     console.log("=====================")
