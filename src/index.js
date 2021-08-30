9188c9f3477ecf931cf7aec3f78b7cdf6491aa7b
Daniel Mesa <damesamarin@gmail.com>
2021-08-30 17:24:57 +0200

    cors all origins

--- src/index.js
+++ src/index.js
@@ -1,20 +1,23 @@
 /*imports*/
-const express= require('express');
-require('dotenv').config({ path: './db.env' });
+const express = require('express');
+require('dotenv').config({path: './db.env'});
+const cors=require('cors');
 
-c.Header("Access-Control-Allow-Origin", "*")
 /*inicializaciones*/
-const app=express();
+const app = express();
+app.use(cors());
+app.options('*',cors());
 
-const PORT=process.env.PORT || 3000
+const PORT = process.env.PORT || 3000
 
-const mongo=require('./database');
+const mongo = require('./database');
+
 app.use(express.json());
 app.use(require('./endpoints/index'));
 /*servidor*/
 
-app.listen(PORT,()=>{
+app.listen(PORT, () => {
     console.log("=====================")
-    console.log("SERVER ON PORT:",PORT)
+    console.log("SERVER ON PORT:", PORT)
     console.log("=====================")
 })
